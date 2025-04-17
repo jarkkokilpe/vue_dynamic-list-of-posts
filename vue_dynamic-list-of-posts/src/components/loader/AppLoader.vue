7f6aec6fce98e1a5f482ca08eecbcbf750d15575
jarkkokilpe <jarkko.kilpelainen@protonmail.com>
2025-04-17 18:31:20 +0300

    Solution

---
+++ vue_dynamic-list-of-posts/src/components/loader/AppLoader.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="app-loader">
+    <div class="spinner"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppLoader',
+};
+</script>
+
+<style scoped>
+.app-loader {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.spinner {
+  border: 4px solid rgba(0, 0, 0, 0.1);
+  border-left-color: #4caf50;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
